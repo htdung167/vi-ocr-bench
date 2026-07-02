7fbcb18d697d1cfdc34c2053747e0c609471e255
dunght50 <v.dunght50@vinsmartfuture.tech>
2026-07-02 11:44:07 +0700

    feat: add script download and serving

--- scripts/download_model.sh
+++ scripts/download_model.sh
@@ -3,12 +3,17 @@
 # Chandra-OCR-2 -> DONE
 # VietOCR -> Anh Khang
 
-# HunyuanOCR
+# tencent/HunyuanOCR (1B)
+HF_HUB_ENABLE_HF_TRANSFER=1 hf download tencent/HunyuanOCR --local-dir ./models/tencent/HunyuanOCR --exclude "original" --token $HF_TOKEN
 
-# Dots.mocr
+# rednote-hilab/dots.mocr (3B)
+HF_HUB_ENABLE_HF_TRANSFER=1 hf download rednote-hilab/dots.mocr --local-dir ./models/rednote-hilab/dots.mocr --exclude "original" --token $HF_TOKEN
 
-# Vintern
+# 5CD-AI/Vintern-1B-v3_5 (1B)
+HF_HUB_ENABLE_HF_TRANSFER=1 hf download 5CD-AI/Vintern-1B-v3_5 --local-dir ./models/5CD-AI/Vintern-1B-v3_5 --exclude "original" --token $HF_TOKEN
 
-# Deepseek-ocr
+# deepseek-ai/DeepSeek-OCR-2 (3B)
+HF_HUB_ENABLE_HF_TRANSFER=1 hf download deepseek-ai/DeepSeek-OCR-2 --local-dir ./models/deepseek-ai/DeepSeek-OCR-2 --exclude "original" --token $HF_TOKEN
 
-# Nanonets-ocr-2
+# nanonets/Nanonets-OCR2-3B (3B)
+HF_HUB_ENABLE_HF_TRANSFER=1 hf download nanonets/Nanonets-OCR2-3B --local-dir ./models/nanonets/Nanonets-OCR2-3B --exclude "original" --token $HF_TOKEN
--- scripts/vllm_serving.sh
+++ scripts/vllm_serving.sh
@@ -3,12 +3,35 @@
 # Chandra-OCR-2 -> DONE
 # VietOCR -> Anh Khang
 
-# HunyuanOCR
+# tencent/HunyuanOCR (1B)
+CUDA_VISIBLE_DEVICES=0 vllm serve models/tencent/HunyuanOCR \
+    --port 8089 \
+    --no-enable-prefix-caching \
+    --mm-processor-cache-gb 0 \
+    --gpu-memory-utilization 0.8
 
-# Dots.mocr
+# # rednote-hilab/dots.mocr (3B)
+# CUDA_VISIBLE_DEVICES=0 vllm serve models/rednote-hilab/dots.mocr \
+#                     --port 8089 \
+#                     --gpu-memory-utilization 0.9 \
+#                     --chat-template-content-format string \
+#                     --trust-remote-code
 
-# Vintern
+# # 5CD-AI/Vintern-1B-v3_5 (1B)
+# CUDA_VISIBLE_DEVICES=0 vllm serve models/5CD-AI/Vintern-1B-v3_5 \
+#                     --port 8089 \
+#                     --gpu-memory-utilization 0.9 \
+#                     --trust-remote-code
 
-# Deepseek-ocr
+# deepseek-ai/DeepSeek-OCR-2 (3B)
+# CUDA_VISIBLE_DEVICES=0 vllm serve deepseek-ai/DeepSeek-OCR-2 \
+#   --trust-remote-code \
+#   --logits_processors vllm.model_executor.models.deepseek_ocr:NGramPerReqLogitsProcessor \
+#   --no-enable-prefix-caching \
+#   --mm-processor-cache-gb 0
 
-# Nanonets-ocr-2
+# nanonets/Nanonets-OCR2-3B (3B)
+# CUDA_VISIBLE_DEVICES=0 vllm serve models/nanonets/Nanonets-OCR2-3B \
+#                     --port 8089 \
+#                     --gpu-memory-utilization 0.9 \
+#                     --trust-remote-code
