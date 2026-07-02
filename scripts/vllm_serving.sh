7fba228bde6f0d474d596620ab40000a3f018987
dunght50 <v.dunght50@vinsmartfuture.tech>
2026-07-02 10:21:59 +0700

    feat: add init scripts

---
+++ scripts/vllm_serving.sh
@@ -0,0 +1,14 @@
+# PaddleOCR PP-OCRv5_server_rec -> DONE
+# PaddleOCR-VL -> DONE
+# Chandra-OCR-2 -> DONE
+# VietOCR -> Anh Khang
+
+# HunyuanOCR
+
+# Dots.mocr
+
+# Vintern
+
+# Deepseek-ocr
+
+# Nanonets-ocr-2
